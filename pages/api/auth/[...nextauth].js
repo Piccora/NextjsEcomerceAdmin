d3dbf50278d3d80fe9d55e7ed322896ad5e2b2c3
bestfizzz <minhhoangnguyen19122004@gmail.com>
2023-06-27 22:19:56 +0700

    Authentication rework

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,28 +1,41 @@
-import NextAuth, { getServerSession } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 import { firestore } from '@/lib/firebase/firestore';
+import storeAnyUser from '@/lib/firebase/storeAnyUser';
 
-const adminEmails = ['minhhoangnguyen19122004@gmail.com','eaplimitedvn@gmail.com','authorizedworker@gmail.com']
+const adminEmails = ['minhhoangnguyen19122004@gmail.com', 'eaplimitedvn@gmail.com', 'authorizedworker@gmail.com'];
 
 const authOptions = {
   adapter: FirestoreAdapter(firestore),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    })
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
   ],
-  callbacks:{
-    session:({token,session,user})=>{
-      if(adminEmails.includes(user.email)){
-        return session
+  callbacks: {
+    session: async ({ session, user }) => {
+      if (adminEmails.includes(user.email)) {
+        return session;
       }
-    }
-  }
-}
 
-export default NextAuth(authOptions)
+      console.log('Not authenticated as admin');
+      return null;
+    },
+    signIn: async ({ user }) => {
+      // Store user account information in your database here
+      storeAnyUser(user);
+
+      if (!adminEmails.includes(user.email)) {
+        return false; // Prevent non-admin users from signing in
+      }
+      return true; // Allow admin users to sign in
+    },
+  },
+};
+
+export default NextAuth(authOptions);
 
 export async function isAdminRequest(req, res) {
   const session = await getServerSession(req,res,authOptions)
